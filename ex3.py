3cd63241a89ae0d0a99bc07981e8b483c8dede0d
idan achituve <idanachi@gmail.com>
2019-01-10 20:02:08 +0200

    2nd

--- ex3.py
+++ ex3.py
@@ -68,7 +68,7 @@ H2_G = 10  # 2nd hidden layer size in the generator
 H1_D = 10  # 1st hidden layer size in the discriminator
 H2_D = 10  # 2nd hidden layer size in the discriminator
 p = 0.2  # dropout probability
-num_epochs = 5000
+num_epochs = 2000
 reg = 0.0001  # regularization
 lr_g = 0.0002  # generator learning rate
 beta_g = 0.5  # Beta1 hyperparam for Adam optimizers
@@ -84,7 +84,7 @@ device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else 
 
 # generate data
 noise = torch.rand(num_samples, D_in, device=device)  # random data uniformly distributed
-real_data = torch.from_numpy(gd.get_data(num_samples, target_dist)).to(device)  # read data
+real_data = torch.from_numpy(gd.get_data(num_samples, target_dist)).float().to(device)  # read data
 
 # create occurrence classs
 netG = Generator(ngpu, D_in, H1_G, H2_G, p).to(device)
@@ -98,8 +98,8 @@ real_label = 1
 fake_label = 0
 
 # Setup Adam optimizers for both G and D
-optimizerD = optim.Adam(netD.parameters(), lr=lr_g, betas=(beta_g, 0.999))
-optimizerG = optim.Adam(netG.parameters(), lr=lr_d, betas=(beta_d, 0.999))
+optimizerD = optim.Adam(netD.parameters(), lr=lr_d, betas=(beta_g, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=lr_g, betas=(beta_d, 0.999))
 
 # Lists to keep track of progress
 points_list = []
@@ -112,48 +112,82 @@ print("Starting Training Loop...")
 for epoch in range(num_epochs):
 
     # get random permutation of the noise and the true data
-    noise_permutation = torch.randperm(noise.size()[0])
+    noise_permutation_dis = torch.randperm(noise.size()[0])
+    noise_permutation_gen = torch.randperm(noise.size()[0])
     real_permutation = torch.randperm(real_data.size()[0])
+    dis_loss = gen_loss = D_x = D_G_z1 = D_G_z2 = gen_num_examples = 0
 
     # for each batch
     for i in range(0, real_data.size()[0], batch_size):
 
-        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+        # (1) Update D network: maximizing log(D(x)) + log(1 - D(G(z)))
+        # An equivalent method is to minimize -(y_t*log(D(x) + (1 - y_t)*log(1 - D(G(z))))) with gradient descent
+        # accumulate errors from genuine and fake examples in the batch and make the update step
+
         netD.train()
         netD.zero_grad()
 
         # get batch examples
-        indices_noise = noise_permutation[i:i + batch_size]
+        indices_noise = noise_permutation_dis[i:i + batch_size]
         indices_real = real_permutation[i:i + batch_size]
         batch_fake = noise[indices_noise]
         batch_real = real_data[indices_real]
+        b_size = batch_real.size(0)
 
         # run true examples through the network
-        label = torch.full((batch_size,), real_label, device=device)  # set labels
+        label = torch.full((b_size,), real_label, device=device)  # set labels
         output = netD(batch_real).view(-1)  # get network output
-        errD_real = criterion(output, label)  # calc. BCE error
+        errD_real = criterion(output, label)  # calc. average BCE error
         errD_real.backward()  # calc gradients for discriminator
-        D_x = output.mean().item()  # average value given to true distribution
+        D_x += output.sum().item()  # average value given to true distribution
 
         # run fake examples through the network
-        label = torch.full((batch_size,), real_label, device=device)  # set labels
-        output = netD(batch_real).view(-1)  # get network output
-        errD_real = criterion(output, label)  # calc. BCE error
-        errD_real.backward()  # calc gradients for discriminator
-        D_x = output.mean().item()  # average value given to true distribution
+        fake = netG(batch_fake)
+        label.fill_(fake_label)  # set labels
+        output = netD(fake.detach()).view(-1)  # get network output without backprop
+        errD_fake = criterion(output, label)  # calc. average BCE error
+        errD_fake.backward()  # calc gradients for discriminator
+        D_G_z1 += output.sum().item()  # average value given to fake distribution
 
+        # Add the gradients from the all-real and all-fake batches
+        errD = errD_real + errD_fake
+        optimizerD.step()
 
-        
+        dis_loss += errD*2*b_size  # get the sum of errors
 
-        netG.train()
-        netG.zero_grad()
+        # (2) Update G network: maximize log(D(G(z)))
+        # Alternative is to minimize -y_t*log(D(G(z))) with labels of "1" and with gradient descent
+        # update generator weights every k steps
+        if i % k == 0:
 
+            netG.train()
+            netG.zero_grad()
 
+            # get batch examples
+            indices_noise = noise_permutation_gen[i:i + batch_size]
+            batch_fake = noise[indices_noise]
+            b_size = batch_fake.size(0)
 
+            label = torch.full((b_size,), real_label, device=device)  # fake labels are real for generator cost
+            fake = netG(batch_fake)
+            output = netD(fake).view(-1)  # get network output
+            errG = criterion(output, label)  # calc. BCE error
+            errG.backward()  # calc gradients for discriminator
+            D_G_z2 += output.sum().item()  # average value given to fake distribution
+            gen_num_examples += b_size
 
+            optimizerG.step()
+            gen_loss += errG*b_size  # get the sum of errors
 
+    # save losses
+    D_losses.append(dis_loss)
+    G_losses.append(gen_loss)
 
-
+    # Output training stats
+    if epoch % 500 == 499:
+        print('[%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+              % (epoch, num_epochs,
+                 D_losses[-1].item(), G_losses[-1].item(), D_x/num_samples, D_G_z1/num_samples, D_G_z2/gen_num_examples))
 
 
 
